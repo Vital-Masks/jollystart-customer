84ea238994923bdbc2192b02dbbeadbee05dd58c
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2023-05-21 00:18:12 +0530

    bug fix

--- components/Sports/Badminton.jsx
+++ components/Sports/Badminton.jsx
@@ -9,20 +9,30 @@ const Badminton = () => {
     <>
       <div className="bg-slate-100">
         <Container className="text-left">
-          <h1 className="text-2xl font-bold text-slate-800 p-5">Badminton</h1>
-          <p className="mt-5 text-slate-600 p-5">
-          Founded in 1948 by the esteemed alumni of Jaffna Hindu College, Jolly Stars Sports Club has proudly served as a symbol of sporting excellence and camaraderie for over seven decades. 
-          As we approach our remarkable 75th anniversary, we reflect upon our rich history and celebrate the countless achievements that have shaped our club.
-
-          Jolly Stars Sports Club exclusively welcomes students who have had the privilege of studying at Jaffna Hindu College, fostering a sense of unity and shared values among our members. 
-          Our club has become a vibrant hub where passionate sports enthusiasts come together to showcase their talents and compete in a variety of thrilling sporting events.
-          </p> 
-          <p className="mt-5 text-slate-600 p-5">
-          We offer a diverse range of sports, catering to the interests and abilities of our members. From the excitement of football and cricket to 
-          the agility of badminton and athletics, the intensity of basketball and volleyball, the precision of table tennis, the speed of hockey, 
-          and the strength of weightlifting, Jolly Stars Sports Club provides a platform for individuals to excel in their chosen disciplines.
+          <h1 className="p-5 text-2xl font-bold text-slate-800">Badminton</h1>
+          <p className="p-5 mt-5 text-slate-600">
+            Founded in 1948 by the esteemed alumni of Jaffna Hindu College,
+            Jolly Stars Sports Club has proudly served as a symbol of sporting
+            excellence and camaraderie for over seven decades. As we approach
+            our remarkable 75th anniversary, we reflect upon our rich history
+            and celebrate the countless achievements that have shaped our club.
+            Jolly Stars Sports Club exclusively welcomes students who have had
+            the privilege of studying at Jaffna Hindu College, fostering a sense
+            of unity and shared values among our members. Our club has become a
+            vibrant hub where passionate sports enthusiasts come together to
+            showcase their talents and compete in a variety of thrilling
+            sporting events.
           </p>
-          <div className="flex justify-center mt-4 p-5">
+          <p className="p-5 mt-5 text-slate-600">
+            We offer a diverse range of sports, catering to the interests and
+            abilities of our members. From the excitement of football and
+            cricket to the agility of badminton and athletics, the intensity of
+            basketball and volleyball, the precision of table tennis, the speed
+            of hockey, and the strength of weightlifting, Jolly Stars Sports
+            Club provides a platform for individuals to excel in their chosen
+            disciplines.
+          </p>
+          <div className="flex justify-center p-5 mt-4">
             <Image
               src={
                 'https://images.unsplash.com/photo-1578742209285-c4dbba4af948?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhvY2tleXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
@@ -34,26 +44,30 @@ const Badminton = () => {
             />
           </div>
 
-          <p className="mt-5 text-slate-600 p-5">
-          Throughout our storied history, our club has consistently produced exceptional sportsmen and athletes 
-          who have made their mark not only within our organization but also at regional and national levels. 
-          Their achievements stand as a testament to the dedication, perseverance, and talent that thrive within the walls of our club.
-
-          At Jolly Stars Sports Club, we pride ourselves on our commitment to excellence both on 
-          and off the field. We strive to be pioneers in the world of sports, setting a benchmark 
-          for achievement and functioning that inspires other clubs to follow suit. We believe in nurturing 
-          the holistic development of our members, encouraging sportsmanship, discipline, teamwork, 
-          and leadership qualities that extend far beyond the realm of sports.
+          <p className="p-5 mt-5 text-slate-600">
+            Throughout our storied history, our club has consistently produced
+            exceptional sportsmen and athletes who have made their mark not only
+            within our organization but also at regional and national levels.
+            Their achievements stand as a testament to the dedication,
+            perseverance, and talent that thrive within the walls of our club.
+            At Jolly Stars Sports Club, we pride ourselves on our commitment to
+            excellence both on and off the field. We strive to be pioneers in
+            the world of sports, setting a benchmark for achievement and
+            functioning that inspires other clubs to follow suit. We believe in
+            nurturing the holistic development of our members, encouraging
+            sportsmanship, discipline, teamwork, and leadership qualities that
+            extend far beyond the realm of sports.
           </p>
-          <p className="mt-5 text-slate-600 p-5">
-          As we embark on our 75th anniversary, we remain steadfast in our mission to provide a platform
-           for Jaffna Hindu College alumni to connect, compete, and flourish. We invite you to join our 
-           esteemed community, where passion meets skill, friendships are forged, and legends are born.
-
-          Come be a part of Jolly Stars Sports Club and witness the spirit of sportsmanship and camaraderie that 
-        has defined us for generations. Together, let's continue to create a legacy that transcends time.
-
-        Jolly Stars Sports Club - Where Champions are Made!
+          <p className="p-5 mt-5 text-slate-600">
+            As we embark on our 75th anniversary, we remain steadfast in our
+            mission to provide a platform for Jaffna Hindu College alumni to
+            connect, compete, and flourish. We invite you to join our esteemed
+            community, where passion meets skill, friendships are forged, and
+            legends are born. Come be a part of Jolly Stars Sports Club and
+            witness the spirit of sportsmanship and camaraderie that has defined
+            us for generations. Together, let&apos;s continue to create a legacy
+            that transcends time. Jolly Stars Sports Club - Where Champions are
+            Made!
           </p>
         </Container>
       </div>
--- pages/cricket/club-cricket.jsx
+++ pages/cricket/club-cricket.jsx
@@ -7,30 +7,36 @@ const ClubCricket = () => {
     <>
       <div className="bg-slate-100">
         <Container className="text-left">
-          <h1 className="text-2xl font-bold text-slate-800 p-5">Club Cricket History</h1>
-          <p className="mt-5 text-slate-600 p-5">
-          Cricket holds a special place in the heart of our esteemed sports club, where the sound of 
-          leather meeting willow echoes through the years. With a passion for the game running deep 
-          within our veins, Jolly Stars Cricket Division stands as a testament to the rich tradition 
-          and unwavering commitment to excellence.
-
-        Within the hallowed grounds of our club, cricket is more than just a sport; it is a way of life. 
-        We provide a nurturing environment for cricketers, honing their skills, and nurturing their passion 
-        for the game. Our cricket division is known for its unwavering dedication to promoting the spirit of 
-        cricket, both on and off the field.
-          </p><br></br>
-          <p className="mt-5 text-slate-600 p-5">
-          At Jolly Stars, we boast state-of-the-art cricket facilities, including well-maintained pitches, 
-          professional coaching staff, and a supportive network of fellow cricketers. Whether you're a 
-          seasoned player or just starting your cricketing journey, our club provides the perfect platform
-           to develop your skills, compete at various levels, and forge lifelong friendships.
-
-          Our cricket division encompasses all formats of the game, including Test matches, One-Day, and 
-          Twenty20 matches. We organize regular practice sessions, intra-club matches, and competitive 
-          tournaments to ensure our cricketers have ample opportunities to showcase their talent and 
-          continually raise the bar of their performance.
+          <h1 className="p-5 text-2xl font-bold text-slate-800">
+            Club Cricket History
+          </h1>
+          <p className="p-5 mt-5 text-slate-600">
+            Cricket holds a special place in the heart of our esteemed sports
+            club, where the sound of leather meeting willow echoes through the
+            years. With a passion for the game running deep within our veins,
+            Jolly Stars Cricket Division stands as a testament to the rich
+            tradition and unwavering commitment to excellence. Within the
+            hallowed grounds of our club, cricket is more than just a sport; it
+            is a way of life. We provide a nurturing environment for cricketers,
+            honing their skills, and nurturing their passion for the game. Our
+            cricket division is known for its unwavering dedication to promoting
+            the spirit of cricket, both on and off the field.
           </p>
-          <div className="flex justify-center mt-4 p-5">
+          <br></br>
+          <p className="p-5 mt-5 text-slate-600">
+            At Jolly Stars, we boast state-of-the-art cricket facilities,
+            including well-maintained pitches, professional coaching staff, and
+            a supportive network of fellow cricketers. Whether you&apos;re a
+            seasoned player or just starting your cricketing journey, our club
+            provides the perfect platform to develop your skills, compete at
+            various levels, and forge lifelong friendships. Our cricket division
+            encompasses all formats of the game, including Test matches,
+            One-Day, and Twenty20 matches. We organize regular practice
+            sessions, intra-club matches, and competitive tournaments to ensure
+            our cricketers have ample opportunities to showcase their talent and
+            continually raise the bar of their performance.
+          </p>
+          <div className="flex justify-center p-5 mt-4">
             <Image
               src={
                 'https://images.unsplash.com/photo-1578742209285-c4dbba4af948?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhvY2tleXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
@@ -41,25 +47,27 @@ const ClubCricket = () => {
               alt="gg"
             />
           </div>
-          <p className="mt-5 text-slate-600 p-5">
-          Jolly Stars Sports Club has produced an impressive lineage of cricketing talent that has 
-          represented not only our club but also the region and the district and province with 
-          distinction. Our players have earned accolades, broken records, and become beacons of 
-          inspiration for aspiring cricketers. We take immense pride in their accomplishments and 
-          continue to provide them with unwavering support and guidance.
-
-          Beyond the boundaries of the game, Jolly Stars Cricket Division instills the values of 
-          sportsmanship, discipline, and teamwork. We believe in nurturing not just exceptional 
-          cricketers but also individuals who exhibit integrity, respect, and leadership both on 
-          and off the field. Our cricketers are ambassadors of fair play and serve as role models for 
-          the cricketing fraternity.
-
-          Join us at Jolly Stars Cricket Division and immerse yourself in the world of cricketing 
-          excellence. Whether you're a young talent dreaming of wearing the national colors or a 
-          passionate fan looking for a community that shares your love for the game, we welcome you 
-          to be a part of our rich heritage and contribute to the glorious legacy of Jolly Stars Cricket. 
-          Together, let us continue to push the boundaries of the game, inspire future generations, and 
-          create unforgettable cricketing moments that will be etched in history.
+          <p className="p-5 mt-5 text-slate-600">
+            Jolly Stars Sports Club has produced an impressive lineage of
+            cricketing talent that has represented not only our club but also
+            the region and the district and province with distinction. Our
+            players have earned accolades, broken records, and become beacons of
+            inspiration for aspiring cricketers. We take immense pride in their
+            accomplishments and continue to provide them with unwavering support
+            and guidance. Beyond the boundaries of the game, Jolly Stars Cricket
+            Division instills the values of sportsmanship, discipline, and
+            teamwork. We believe in nurturing not just exceptional cricketers
+            but also individuals who exhibit integrity, respect, and leadership
+            both on and off the field. Our cricketers are ambassadors of fair
+            play and serve as role models for the cricketing fraternity. Join us
+            at Jolly Stars Cricket Division and immerse yourself in the world of
+            cricketing excellence. Whether you&apos;re a young talent dreaming
+            of wearing the national colors or a passionate fan looking for a
+            community that shares your love for the game, we welcome you to be a
+            part of our rich heritage and contribute to the glorious legacy of
+            Jolly Stars Cricket. Together, let us continue to push the
+            boundaries of the game, inspire future generations, and create
+            unforgettable cricketing moments that will be etched in history.
           </p>
         </Container>
       </div>
