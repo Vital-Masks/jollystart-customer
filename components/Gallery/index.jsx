a84fba29046cafcf95a386c289e5cb8ebf998143
Benedict Washington -X (bewashin - HCL TECHNOLOGIES LIMITED at Cisco) <bewashin@cisco.com>
2023-05-20 23:41:51 +0530

    images

--- components/Gallery/index.jsx
+++ components/Gallery/index.jsx
@@ -3,6 +3,32 @@ import Container from '../Shared/Container';
 import Image from 'next/image';
 
 const Gallery = () => {
+  const images = [
+    {
+      id: 1,
+      image: '/assets/banner/gallery1.jpg',
+    },
+    {
+      id: 2,
+      image: '/assets/banner/gallery2.jpg',
+    },
+    {
+      id: 3,
+      image: '/assets/banner/gallery3.jpg',
+    },
+    {
+      id: 4,
+      image: '/assets/banner/gallery4.jpg',
+    },
+    {
+      id: 5,
+      image: '/assets/banner/gallery5.jpg',
+    },
+    {
+      id: 6,
+      image: '/assets/banner/gallery6.jpg',
+    },
+  ];
   return (
     <div className="bg-slate-200">
       <Container>
@@ -15,10 +41,10 @@ const Gallery = () => {
           </p>
         </div>
         <div className="flex grid-cols-2 gap-2 overflow-auto md:grid md:grid-cols-3 lg:grid-cols-6 p-5">
-          {[1, 2, 3, 4, 5, 6].map((img) => (
-            <div key={img} className="w-full shrink-0">
+          {images.map((img) => (
+            <div key={img.id} className="w-full shrink-0">
               <Image
-                src="/assets/banner/gallery1.jpg"
+                src={img.image}
                 width={720}
                 height={720}
                 className="w-full h-full aspect-square"
