9b640ea176daf97f2f53844e65035cb393df3283
IT21150098 <it21150098@my.sliit.lk>
2023-12-26 18:18:56 +0530

    file fetch done

--- components/Info/Clubfiles.jsx
+++ components/Info/Clubfiles.jsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import Footer from '@/components/Footer';
-import HomeBanner from '@/components/HomeBanner';
-import Container from '@/components/Shared/Container';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import Footer from "@/components/Footer";
+import HomeBanner from "@/components/HomeBanner";
+import Container from "@/components/Shared/Container";
+import Image from "next/image";
+import Link from "next/link";
+import DateDisplay from "@/utils/dateDisplay";
 
 const Clubfile = () => {
+  //
+  const [MemberFileData, setMemberFileData] = useState([]);
+  const [Loading, setLoading] = useState(true);
+  const fetchUserData = () => {
+    try {
+      fetch(`http://localhost:3000/api/fileManagement/getAllFiles`)
+        .then((fetchResponse) => {
+          // Check if the fetch request was successful
+          if (fetchResponse.ok) {
+            // Process the response here, e.g., parse the JSON response
+            console.log(fetchResponse, "==========");
+            return fetchResponse.json();
+          } else {
+            // Handle fetch error
+            throw new Error("Fetch request failed");
+          }
+        })
+        .then((userDataFromFetch) => {
+          console.log(userDataFromFetch.result);
+          setMemberFileData(userDataFromFetch.result);
+          setLoading(false);
+        })
+        .catch((error) => {
+          // Handle fetch error or redirect to login page
+          console.error("Fetch error:", error);
+          setLoading(false);
+          // redirectToLoginPage();
+        });
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <div className="bg-slate-100">
+    <div className="">
       <Container>
         <div className="mb-5 text-center">
           <h1 className="text-2xl font-bold text-slate-800">Club Files</h1>
-          
         </div>
-        <div className="flex items-center justify-start gap-5 overflow-auto lg:grid-cols-2 xl:grid-cols-4 md:grid md:grid-cols-2 lg:justify-center p-5">
+        {Loading ? (
+          <div className="loaderBody">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div>
+            <div className="flex items-center justify-start gap-5 overflow-auto lg:grid-cols-2 xl:grid-cols-4 md:grid md:grid-cols-2 lg:justify-center p-5">
+              {MemberFileData &&
+                MemberFileData.map((item, i) => (
+                  <ClubfileCard key={i} filedata={item} />
+                ))}
+            </div>
+          </div>
+        )}
+
+        {/* <ClubfileCard />
           <ClubfileCard />
-          <ClubfileCard />
-          <ClubfileCard />
-          <ClubfileCard />
-        </div>
+          <ClubfileCard /> */}
 
         <div className="text-center">
           <button className="content-center px-4 py-2 mt-10 text-white bg-blue-900 rounded-md">
@@ -32,27 +81,44 @@ const Clubfile = () => {
 
 export default Clubfile;
 
-const ClubfileCard = () => {
+const ClubfileCard = ({ filedata }) => {
+  const { _id, title, description, file, created_at } = filedata;
+  //   {
+  //     "_id": "6553d4cfc1205cd2ae847eec",
+  //     "title": "jghjgh",
+  //     "description": "jghjghjghjh",
+  //     "file": "1699992783402-10298117.pdf",
+  //     "isDeleted": false,
+  //     "created_at": "2023-11-14T20:13:03.427Z",
+  //     "updated_at": "2023-11-14T20:13:03.427Z",
+  //     "__v": 0
+  // },
+  const openFileInChrome = () => {
+    if (file) {
+      const fileUrl = "http://localhost:3000/files/" + file;
+      window.open(fileUrl, "_blank");
+    }
+  };
+
   return (
-    
     <div className="w-full max-w-xs border-2 rounded-lg shrink-0">
-      
       <div className="flex items-center justify-center h-40 rounded-t-lg bg-slate-400">
-      <h1 className="mb-5 text-xl font-bold">
-          Account Audit 2022
-        </h1>
+        <h1 className="mb-5 text-xl font-bold">{title}</h1>
       </div>
       <div className="p-5 text-black">
-        <div className="mb-5 text-sm text-slate-400"> 2023 July 10th</div>
-        <div className="mb-5 text-sm">
-          <p>
-            The membership of the club is open to all persons who are approved
-            by the Executive Committee. Each applicant for admission as a member
-          </p>
+        <div className="mb-5 text-sm text-slate-400">
+          <DateDisplay dateString={created_at} />
         </div>
-        <Link href="#" className="mb-5 text-sm font-bold text-blue-900">
+        <div className="mb-5 text-sm">
+          <p>{description}</p>
+        </div>
+        <div
+          onClick={openFileInChrome}
+          href="#"
+          className="mb-5 text-sm font-bold text-blue-900"
+        >
           View File
-        </Link>
+        </div>
       </div>
     </div>
   );
