3cf202ca61fb427b8bff95b73167de88b9e131c6
IT21150098 <it21150098@my.sliit.lk>
2024-10-09 16:21:50 +0530

    upload

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com','images.saymedia-content.com',"e0.pxfuel.com","encrypted-tbn0.gstatic.com","live.staticflickr.com"],
+    domains: [
+      "images.unsplash.com",
+      "images.saymedia-content.com",
+      "e0.pxfuel.com",
+      "encrypted-tbn0.gstatic.com",
+      "live.staticflickr.com",
+    ],
+    unoptimized: true, // Disable image optimization for static export
   },
+  
+  output: "export", // Ensure static HTML export is enabled
 };
 
 module.exports = nextConfig;
