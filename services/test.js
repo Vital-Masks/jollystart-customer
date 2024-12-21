e532ee3e78d68b27d29680893e0a4207d86e7f0b
Vetha <vitalmasks@gmail.com>
2024-12-21 15:03:22 +0530

    test1

---
+++ services/test.js
@@ -0,0 +1,52 @@
+{
+    "_id": "676530eaea9980f3f693680d",
+    "membershipCategory": "ORDINARY MEMBERS",
+    "profilePicture": "",
+    "title": "Mr",
+    "firstName": "qq",
+    "lastName": "qqq",
+    "dateOfBirth": "2024-12-18",
+    "passportNumber": "993121517V",
+    "email": "q@gmail.com",
+    "userName": "qq",
+    "password": "q@gmail.com",
+    "phoneNumber": "0751711215",
+    "telephoneNumber": "0751711215",
+    "address": "3rd Junction Kerudavil South Thondaimanaru",
+    "maritalStatus": "Married",
+    "workPlaceName": "q",
+    "occupation": "q",
+    "officeAddress": "q",
+    "schoolDetails": [
+        {
+            "schoolName": "1",
+            "participated": "2",
+            "game": "3",
+            "from": "4",
+            "to": "5",
+            "role": "6",
+            "_id": "676530eaea9980f3f693680e"
+        }
+    ],
+    "clubDetails": [
+        {
+            "clubName": "11",
+            "involved": "22",
+            "game": "33",
+            "from": "44",
+            "to": "55",
+            "role": "66",
+            "_id": "676530eaea9980f3f693680f"
+        }
+    ],
+    "gallery": [
+      "s"    ],
+    "isSchoolDetailVerified": false,
+    "isPaymentDetailVerified": false,
+    "memberApprovalStatus": "REMOVED",
+    "membershipId": "JSSC1919",
+    "declinedMessage": "wrong info",
+    "created_at": "2024-12-20T08:55:06.527Z",
+    "updated_at": "2024-12-20T09:09:57.589Z",
+    "__v": 0
+}
