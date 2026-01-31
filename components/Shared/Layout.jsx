49735f7a5e52e0fda7020532e0b7f07bad77990e
Yathushan@ticketraa <ticketraa@gmail.com>
2026-01-31 21:43:31 +0530

    build fix

--- components/Shared/Layout.jsx
+++ components/Shared/Layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './Header';
-import { Inter } from 'next/font/google';
 import Footer from '../Footer';
-const inter = Inter({ subsets: ['latin'] });
 
 const Layout = ({ children }) => {
   return (
-    <div className={`${inter.className}`}>
+    <div className="font-inter">
       <Header />
       <main
       // className="min-h-[calc(100vh-11rem)]"
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/ag-grid-community@31.0.1/styles/ag-grid.css"
